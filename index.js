16d6247494724590718d03f4b9d88ddea52fe7f9
Trần Quốc Đạt <quocdat23894@gmail.com>
2018-12-24 03:43:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
+  if (string == "I love you, Grandma.") {
+    return "I love you, too."
+  }
   if (string == whisper(string)) {
     return "I can't hear you!"
   } else {
