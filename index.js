7ec492d9fba2224768ed3751099ed19b3f6c2e9c
Trần Quốc Đạt <quocdat23894@gmail.com>
2018-12-24 03:40:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string){
   if (isLowerCase(string)) {
-    return sting.toLowerCase()
+    return "YES INDEED!"
   } else {
     return "I can't hear you!"
   }
